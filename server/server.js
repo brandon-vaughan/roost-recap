353facd1816152e3ce24e5496590b8a91d6e6930
Brandon Vaughan <brandon.vaughan@nclud.com>
2012-12-07 14:01:08 -0500

    added config object for server settings

--- server/server.js
+++ server/server.js
@@ -7,11 +7,11 @@
 =            Dependencies            =
 ====================================*/
 
+// Config: Config object with server settings
+var config = require( './config.js' );
+
 // Express(bodyParser()): An advanced html framework
 var express = require( 'express' );
-
-// Socket.io: Real time node module for events 
-var io = require( 'socket.io' );
 
 // FS: Node module for file read and write
 var fs = require( 'fs' );
@@ -19,14 +19,14 @@ var fs = require( 'fs' );
 // App: Instantiate Express( bodyParser() ); bodyParser wrapes for JSON, urlencoded, and multipart 
 var app = express( express.bodyParser() );
 
-// Server.listen(port): Instantiate node server with app framework; port >= 1080
-var server = require('http').createServer( app );
+// Server.listen(port): Instantiate node server with app framework
+var server = require( 'http' ).createServer( app );
 
-// IO: create socket.io instance;
-io.listen( server );
+// Socket.io: Real time node module for events 
+var io = require( 'socket.io' ).listen( server );
 
-// baseDir: for routes and loading
-var baseDir   = __dirname + '/../';
+// Set server port
+server.listen( config.port );
 
 /*====================================================
 =            Node Server Constructor            =
@@ -34,10 +34,16 @@ var baseDir   = __dirname + '/../';
 module.exports = function() {
 
   /*==========  Create Socket.io Connection  ==========*/
-  
+  io.sockets.on('connection', function( socket ) {
+
+  });  
 
   /*==========  Create App Routes  ==========*/
 
   // Unit Testing
+  app.use( '/_test', express.static( config.baseDir + 'test') );
+
+  // Running Server Message
+  console.log( 'Serving on ' + config.baseUrl + ':' + config.port );
 
 };
