93f9716255e4b144b7367a24697d42034bbf7f9c
Brandon Vaughan <brandon.vaughan@nclud.com>
2012-12-07 14:01:31 -0500

    added config module for server config settings

---
+++ server/config.js
@@ -0,0 +1,18 @@
+/*=====================================
+=            Config Module            =
+=====================================*/
+
+module.exports = (function() {
+
+  // Config Object with default settings
+  var config = {
+    baseUrl: 'http://liveprez',
+    baseDir: __dirname + '/../',
+    port: 8000
+  };
+
+  return config;
+
+}());
+
+
