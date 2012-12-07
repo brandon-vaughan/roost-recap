b235b787ecbf2b5fb628bf75f6cd42b6c88a2417
Brandon Vaughan <brandon.vaughan@nclud.com>
2012-12-07 12:33:39 -0500

    Defining node server

---
+++ server/server.js
@@ -0,0 +1,24 @@
+/**
+ * Node Socket.io
+ * 
+ */
+
+/*====================================
+=            Dependencies            =
+====================================*/
+//
+// Express(bodyParser()): An advanced html framework
+// Socket.io: Real time node module for events 
+// FS: Node module for file read and write
+
+// App: Instantiate Express( bodyParser() ); bodyParser wrapes for JSON, urlencoded, and multipart 
+// Server.listen(port): Instantiate node server with app framework; port >= 1080
+// IO: create socket.io instance;
+// baseDir: for routes and loading
+
+/*====================================================
+=            Socket.io Server Constructor            =
+====================================================*/
+module.exports = function() {
+
+};
